6fff4ab2159a39f273143f7ad0e6dd4c90cf3402
Abiola <abioladave24@gmail.com>
2025-01-28 14:22:03 +0100

    first commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
     "url": "index.html",
-    "revision": "0.jbb69iac2ug"
+    "revision": "0.utp1ab12f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
